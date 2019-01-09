80ae7b9f5f7c582eeac46492270433d6ad88a726
Connor Holloway <connor.holloway@warwick.ac.uk>
2019-01-09 15:43:23 +0000

    Done

--- src/Lab1.hs
+++ src/Lab1.hs
@@ -19,39 +19,42 @@ import Prelude hiding (not, and, max)
 
 -- 0. Define `age` to be your age.
 
-age = 27
+age = 42
 
 -- 1. Define `name` to be your name.
 
-name = "Michael"
+name = "Bob"
 
 -- 2. Modify the definition of `triple` so that it triples its argument.
 
-triple = \x -> x * 2
+triple = \x -> x * 3
 
 -- 3. Replace `undefined` in the definition of `tripleV2` below so that
 -- it behaves like `triple` above.
 
-tripleV2 x = undefined
+tripleV2 x = x * 3
 
 -- 4. Replace `undefined` in the definition of `not` below to implement
 -- boolean negation.
 
-not = undefined
+not x = case x of
+  True -> False
+  False -> True
 
 -- 5. Replace `undefined` in the definition of `and` below to implement
 -- boolean conjunction.
 
-and = undefined
+and x y = x && y
 
 -- 6. Replace `undefined` in the definition of `max` below so that it reduces
 -- to the greater number of `x` and `y`.
 
-max x y = undefined
+max x y | x < y = y
+        | otherwise = x
 
 -- 7. Replace `undefined` in the definition of `perimeterRect` so that it
 -- calculates the perimeter of a rectangle given its length and width.
 
-perimeterRect l w = undefined
+perimeterRect l w = l * 2 + w * 2
 
 --------------------------------------------------------------------------------
